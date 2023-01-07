ff47e0b6ac192e7c016d4cb53cd89f425d61e679
Widya Ade Bagus <dewidyabagus@gmail.com>
2023-01-07 10:18:59 +0700

    Init repository

---
+++ Introduction/hello_world.go
@@ -0,0 +1,17 @@
+package main // Main package
+
+// Single import package
+// Ex: Import standart output package
+import "fmt"
+
+// Multi import package
+// import (
+// 	"package1"
+// 	"package2"
+// )
+
+// Main function
+func main() {
+	// Print "Hi, Hello World" to console
+	fmt.Println("Hi, Hello World")
+}
